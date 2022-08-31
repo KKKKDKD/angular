615142b7017312b86d7eb3a419c3d07cd3539d51
AmirShakur <biniamir88@gmail.com>
2022-08-30 22:05:38 -0500

    edit hiding table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo2';
+  msg: string ='';
+  enterValue=" ";
+  authMsg="";
+  showtable:boolean=false;
+  hidetable:boolean=true;
+  public welcome():void{
+    this.msg = "your file is lood seccessfully looded"
+  }
+  public clear(): void{
+    this.msg =""
+     
+  }
+  public showdata(uname:string,password:string): void{
+    this.enterValue=uname;
+    this.enterValue=password;
+  }
+
+  public authenticate(uname:string,password:string): void{
+    if (uname=="amir"&& password=="test") {
+      this.authMsg='welcome'+" "+uname;
+       }else{
+        this.authMsg="pls try again";
+       }
+  }
+
+  public showhide(): void{
+    this.showtable=!this.showtable;
+    this.hidetable=!this.hidetable;
+
+  }
 }
